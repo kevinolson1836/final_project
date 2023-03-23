f226f7c806eb85791d081b644a717acf7ea5849b
stevenbrust <113395410+stevenbrust@users.noreply.github.com>
2023-03-22 19:22:59 -0500

    Color Wheel

--- js/sketch.js
+++ js/sketch.js
@@ -1,18 +1,5 @@
-let colorInput = document.querySelector('#color');
-let hexInput = document.querySelector('#hex');
-
-colorInput.addEventListener('input', () =>{
-  let color = colorInput.value;
-  hexInput.value = color;
-  document.body.style.backgroundColor = color;
-} );
 
 let clearbtn;
-let strokeBlue;
-let strokeRed;
-let strokeGreen;
-let strokeBlack;
-
 let strokeSize = 10;
 
 let onColor = '#abafae';
@@ -22,114 +9,13 @@ let parentDiv = "drawDiv";
 
 function setup() {
   
-  var myCanvas = createCanvas(512,512);
+  var canvasDiv = document.getElementById('drawDiv');
+  var width = canvasDiv.offsetWidth;
+
+
+  var myCanvas = createCanvas(width,512);
   myCanvas.parent(parentDiv);
   background(200);
-
-  // clear screen
-  clearbtn = createButton('Clear');
-  clearbtn.parent(parentDiv);
-  clearbtn.style( 'display', 'block');
-  clearbtn.style( 'margin-left', '150px');
-  clearbtn.style('background-color', 'red');
-  clearbtn.mousePressed(function (){
-    background(200);
-
-    clearAllColor();
-  })
-
-
-   // Increase Brush size
-   increaseSize = createButton('Increase Brush');
-   increaseSize.parent(parentDiv);
-   increaseSize.mousePressed(function (){
-    // background(200);
-
-    strokeSize+=1;      
-   })
-
-
-  // Decrease Brush size
-  decreaseSize = createButton('Decrease Brush');
-  decreaseSize.parent(parentDiv);
-  decreaseSize.mousePressed(function (){
-    strokeSize-=1;      
-    if(strokeSize <= 0){
-    strokeSize = 1;      
-    }
-  })
-
-
-
-  // Blue stroke
-  strokeBlue = createButton('Blue');
-  strokeBlue.parent(parentDiv);
-  strokeBlue.mousePressed(function (){
-    stroke(0,0,255);
-
-    clearAllColor();
-    changeColor(strokeBlue);
-
-  })
-
-
-
-  // Red stroke
-  strokeRed = createButton('Red');
-  strokeRed.parent(parentDiv);
-  strokeRed.mousePressed(function (){
-    stroke(255,0,0);
-
-    clearAllColor();
-    changeColor(strokeRed);
-  })
-
-
-
-  // Green stroke
-  strokeGreen = createButton('Green');
-  strokeGreen.parent(parentDiv);
-  strokeGreen.mousePressed(function (){
-    stroke(0,255,0);
-
-    clearAllColor();
-    changeColor(strokeGreen);
-  })
-
-
-
-  // Black stroke
-  strokeBlack = createButton('Black');
-  strokeBlack.parent(parentDiv);
-  strokeBlack.mousePressed(function (){
-    stroke(0,0,0);
-
-    clearAllColor();
-    changeColor(strokeBlack);
-  })
-
-
-
-  // White stroke
-  strokeWhite = createButton('White');
-  strokeWhite.parent(parentDiv);
-  strokeWhite.mousePressed(function (){
-    stroke(255,255,255);
-
-    clearAllColor();
-    changeColor(strokeWhite);
-  })
-
-
-  // White stroke
-  saveBtn = createButton('Save');
-  saveBtn.parent(parentDiv);
-  saveBtn.mousePressed(function (){
-    stroke(255,255,255);
-
-    clearAllColor();
-    changeColor(strokeWhite);
-  })
 
 }
 
@@ -150,4 +36,29 @@ function clearAllColor(){
 
 function changeColor(element){
 	element.style('background-color', onColor);
+}
+
+function increaseSize(){
+  strokeSize +=1;
+}
+
+function decreaseSize(){
+  strokeSize -=1;
+  if(strokeSize <= 0){
+     strokeSize = 1;      
+     }
+}
+
+
+function clearDrawing (){
+  background(200);
+}
+
+function changeColor(){
+  let colorInput = document.querySelector('#color');
+  let hexInput = document.querySelector('#hex');
+  let color = colorInput.value;
+  console.log(color);
+  stroke(color);
+
 }
